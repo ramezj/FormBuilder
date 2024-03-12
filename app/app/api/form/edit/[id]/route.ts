aa3495e1e6282bf9769ae56c93de902b2dcae1c6
daddylunix <ramezjoseph8@gmail.com>
2024-03-12 13:37:56 +0200

    removed api routes, just Server actions for now

--- app/app/api/form/edit/[id]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
-
-export async function  POST(request: Request,{params}:{params:{id:string}}) {
-    const id = params.id;
-}
